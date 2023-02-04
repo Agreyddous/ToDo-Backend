541d959c3b07520c68308d3643979c4764fab672
NandoGomes <fernandovbmgomes@hotmail.com>
2023-02-04 00:45:40 -0300

    Adding required constraing to properties

--- ToDo.Domain.Infra/Configurations/ToDoItemConfiguration.cs
+++ ToDo.Domain.Infra/Configurations/ToDoItemConfiguration.cs
@@ -14,12 +14,12 @@ public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
 		builder.ToTable("ToDoItems");
 		builder.HasKey(toDoItem => toDoItem.Id);
 
-		builder.Property(toDoItem => toDoItem.User).HasMaxLength(120);
-		builder.Property(toDoItem => toDoItem.Title);
-		builder.Property(toDoItem => toDoItem.Description);
-		builder.Property(toDoItem => toDoItem.IsComplete);
-		builder.Property(toDoItem => toDoItem.CreatedAt);
-		builder.Property(toDoItem => toDoItem.LastUpdatedAt);
+		builder.Property(toDoItem => toDoItem.User).IsRequired().HasMaxLength(120);
+		builder.Property(toDoItem => toDoItem.Title).IsRequired();
+		builder.Property(toDoItem => toDoItem.Description).IsRequired();
+		builder.Property(toDoItem => toDoItem.IsComplete).IsRequired();
+		builder.Property(toDoItem => toDoItem.CreatedAt).IsRequired();
+		builder.Property(toDoItem => toDoItem.LastUpdatedAt).IsRequired();
 		builder.Property(toDoItem => toDoItem.CompletedAt);
 
 		builder.HasIndex(toDoItem => toDoItem.IsComplete);
