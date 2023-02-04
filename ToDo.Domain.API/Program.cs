79a7d87fbe415cc492786be6e2bb1020a694a490
NandoGomes <fernandovbmgomes@hotmail.com>
2023-02-03 22:16:48 -0300

    Dependencies injection

--- ToDo.Domain.API/Program.cs
+++ ToDo.Domain.API/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using ToDo.Domain.Handlers;
+using ToDo.Domain.Infra.Contexts;
+using ToDo.Domain.Infra.Repositories;
+using ToDo.Domain.Repositories;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject Entity Framework Data Contexts
+builder.Services.AddDbContext<ToDoDataContext>(options => options.UseInMemoryDatabase("ToDo"));
+// builder.Services.AddDbContext<ToDoDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("toDoConnectionString")));
+
+// Inject Handlers
+builder.Services.AddTransient<ToDoItemHandler>();
+
+// Inject Repositories
+builder.Services.AddTransient<IToDoItemRepository, ToDoItemRepository>();
 
 WebApplication app = builder.Build();
 
