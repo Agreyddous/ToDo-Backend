a10c3e134d043bfc615ef100f72eb4870d5c1393
NandoGomes <fernandovbmgomes@hotmail.com>
2023-02-03 22:08:22 -0300

    Include queries into To-Do Item's repository

--- ToDo.Domain/Repositories/IToDoItemRepository.cs
+++ ToDo.Domain/Repositories/IToDoItemRepository.cs
@@ -12,7 +12,7 @@ public interface IToDoItemRepository
 	/// </summary>
 	/// <param name="id">Unique Identifier corresponding to an existing To-Do Item</param>
 	/// <returns>To-Do Item</returns>
-	ToDoItem Get(Guid id);
+	ToDoItem? Get(Guid id);
 
 	/// <summary>
 	/// Creates a new To-Do Item in the repository
@@ -25,4 +25,30 @@ public interface IToDoItemRepository
 	/// </summary>
 	/// <param name="toDoItem">To-Do Item to be updated</param>
 	void Update(ToDoItem toDoItem);
+
+	/// <summary>
+	/// Returns all To-Do Items of a certain user
+	/// </summary>
+	/// <param name="user">User reference</param>
+	IEnumerable<ToDoItem> GetAll(string user);
+
+	/// <summary>
+	/// Returns all completed To-Do Items of a certain user
+	/// </summary>
+	/// <param name="user">User reference</param>
+	IEnumerable<ToDoItem> GetAllComplete(string user);
+
+	/// <summary>
+	/// Returns all incomplete To-Do Items of a certain user
+	/// </summary>
+	/// <param name="user">User reference</param>
+	IEnumerable<ToDoItem> GetAllIncomplete(string user);
+
+	/// <summary>
+	/// Returns all To-Do Items of a certain user created between the provided Start and End dates
+	/// </summary>
+	/// <param name="user">User reference</param>
+	/// <param name="startDate">Start of the searching period</param>
+	/// <param name="endDate">End of the searching period</param>
+	IEnumerable<ToDoItem> GetCreatedBetween(string user, DateTime startDate, DateTime endDate);
 }
