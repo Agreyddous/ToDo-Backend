38e99f01e1d1965a175cf04a9cc117d91a8a70aa
NandoGomes <fernandovbmgomes@hotmail.com>
2023-02-07 20:01:59 -0300

    Using authorization for To-Do Items controller

--- ToDo.API/Controllers/ToDoItemController.cs
+++ ToDo.API/Controllers/ToDoItemController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ToDo.Domain.Commands;
 using ToDo.Domain.Commands.Contracts;
@@ -7,31 +8,45 @@ using ToDo.Domain.Repositories;
 
 namespace ToDo.API.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("v1/ToDoItems")]
 public class ToDoItemController : ControllerBase
 {
 	[HttpPost("")]
-	public ICommandResult Create([FromServices] ToDoItemHandler toDoItemHandler, CreateToDoItemCommand command) => toDoItemHandler.Handle(command);
+	public ICommandResult Create([FromServices] ToDoItemHandler toDoItemHandler, CreateToDoItemCommand command)
+	{
+		command.SetUser(_retrieveUserId());
 
-	[HttpPut("")]
-	public ICommandResult Update([FromServices] ToDoItemHandler toDoItemHandler, UpdateToDoItemCommand command) => toDoItemHandler.Handle(command);
+		return toDoItemHandler.Handle(command);
+	}
+
+	[HttpPut("{id}")]
+	public ICommandResult Update([FromServices] ToDoItemHandler toDoItemHandler, Guid id, UpdateToDoItemCommand command)
+	{
+		command.SetUser(_retrieveUserId());
+		command.SetId(id);
+
+		return toDoItemHandler.Handle(command);
+	}
 
 	[HttpPost("{id}/Complete")]
-	public ICommandResult Completed([FromServices] ToDoItemHandler toDoItemHandler, Guid id, CompleteToDoItemCommand command) => toDoItemHandler.Handle(command);
+	public ICommandResult Completed([FromServices] ToDoItemHandler toDoItemHandler, Guid id) => toDoItemHandler.Handle(new CompleteToDoItemCommand(id, _retrieveUserId()));
 
 	[HttpPost("{id}/Undo")]
-	public ICommandResult Uncompleted([FromServices] ToDoItemHandler toDoItemHandler, Guid id, UndoCompleteToDoItemCommand command) => toDoItemHandler.Handle(command);
+	public ICommandResult Uncompleted([FromServices] ToDoItemHandler toDoItemHandler, Guid id) => toDoItemHandler.Handle(new UndoCompleteToDoItemCommand(id, _retrieveUserId()));
 
 	[HttpGet("")]
-	public IEnumerable<ToDoItem> GetAll([FromServices] IToDoItemRepository toDoItemRepository, string user) => toDoItemRepository.GetAll(user);
+	public IEnumerable<ToDoItem> GetAll([FromServices] IToDoItemRepository toDoItemRepository) => toDoItemRepository.GetAll(_retrieveUserId());
 
 	[HttpGet("Complete")]
-	public IEnumerable<ToDoItem> GetAllComplete([FromServices] IToDoItemRepository toDoItemRepository, string user) => toDoItemRepository.GetAllComplete(user);
+	public IEnumerable<ToDoItem> GetAllComplete([FromServices] IToDoItemRepository toDoItemRepository) => toDoItemRepository.GetAllComplete(_retrieveUserId());
 
 	[HttpGet("Complete/Today")]
-	public IEnumerable<ToDoItem> GetAllCompleteToday([FromServices] IToDoItemRepository toDoItemRepository, string user) => toDoItemRepository.GetLastUpdatedBetween(user, DateTime.Today.ToUniversalTime(), DateTime.UtcNow, true);
+	public IEnumerable<ToDoItem> GetAllCompleteToday([FromServices] IToDoItemRepository toDoItemRepository) => toDoItemRepository.GetLastUpdatedBetween(_retrieveUserId(), DateTime.Today.ToUniversalTime(), DateTime.UtcNow, true);
 
 	[HttpGet("Incomplete")]
-	public IEnumerable<ToDoItem> GetAllIncomplete([FromServices] IToDoItemRepository toDoItemRepository, string user) => toDoItemRepository.GetAllIncomplete(user);
+	public IEnumerable<ToDoItem> GetAllIncomplete([FromServices] IToDoItemRepository toDoItemRepository) => toDoItemRepository.GetAllIncomplete(_retrieveUserId());
+
+	private string _retrieveUserId() => User.Claims.FirstOrDefault(claim => claim.Type == "user_id")?.Value ?? string.Empty;
 }
